0f8452b5e597325edc841c521d1e208c42ee0369
blusk1n <tahaznazen@gmail.com>
2019-11-20 17:01:46 +0100

    stack and queue minimum required

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,35 @@
 var Queue = function() {
+  var tail = 1;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var reorderStorage = function (obj) {
+    for (var key in obj) {
+      obj[key - 1] = obj[key];
+      if (obj[key + 1] === undefined) {
+        delete obj[key];
+      }
+    }
+    return obj;
+  };
   // Implement the methods below
+  someInstance.storage = storage;
 
   someInstance.enqueue = function(value) {
+    storage[tail] = value;
+    tail++;
   };
 
   someInstance.dequeue = function() {
+    var removed = storage[1];
+    delete storage[1];
+    reorderStorage(storage);
+    tail--;
+    return removed;
   };
-
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
 var Stack = function() {
   var someInstance = {};
-
+  var tail = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    tail++;
+    storage[tail] = value;
   };
 
   someInstance.pop = function() {
+    var removed = storage[tail];
+    delete storage[tail];
+    tail --;
+    return removed;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
