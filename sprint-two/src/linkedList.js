d3596c2162846a966b0e44fd4ab475657722aed6
blusk1n <tahaznazen@gmail.com>
2019-11-21 22:01:10 +0100

    2 problems from part 2 solved

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value)
+    if(!list.head){
+      list.head = node;
+    }
+    if(list.tail){
+      list.tail.next = node
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    function innerFunction(obj){
+      if(obj.value === target){
+        return true;
+      }
+      if(obj.next === null){
+        return false;
+      }
+      return innerFunction(obj.next)
+    }
+    return innerFunction(list.head)
+
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,35 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods)
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	this.children.push(new Tree(value));
 };
 
 treeMethods.contains = function(target) {
+	function innerFunction(obj, result){
+		result = result || false;
+		if(obj.value === target){
+			return result = true;
+		}else if(obj.children.length){
+			obj.children.forEach(function(elm){
+				if(!result){
+				result = innerFunction(elm, result);
+				}else{
+					return result;
+				}
+			});	
+		}
+		return result
+	}
+	return innerFunction(this)
 };
 
 
